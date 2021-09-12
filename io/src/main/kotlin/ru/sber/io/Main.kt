1c8b89608985ae5f79a368904f8cc791c5bba297
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-09-12 05:57:52 +0300

    io

--- io/src/main/kotlin/ru/sber/io/Main.kt
+++ io/src/main/kotlin/ru/sber/io/Main.kt
@@ -2,7 +2,6 @@ package ru.sber.io
 
 fun main() {
     val archivator = Archivator()
-
     archivator.zipLogfile()
     archivator.unzipLogfile()
 }
---
+++ io/src/main/kotlin/ru/sber/nio/Main.kt
@@ -0,0 +1,6 @@
+package ru.sber.nio
+
+fun main() {
+    val grep = Grep()
+    grep.find("22/Jan/2001:14:27:46")
+}
