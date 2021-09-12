e0f11f64149220d549ab386718bb90a628748b7d
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-09-12 23:35:57 +0300

    nio

--- io/src/main/kotlin/ru/sber/nio/Grep.kt
+++ io/src/main/kotlin/ru/sber/nio/Grep.kt
@@ -1,5 +1,10 @@
 package ru.sber.nio
 
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.io.path.useLines
+
 /**
  * Реализовать простой аналог утилиты grep с использованием калссов из пакета java.nio.
  */
@@ -15,6 +20,21 @@ class Grep {
      * 22-01-2001-1.log : 3 : 192.168.1.1 - - [22/Jan/2001:14:27:46 +0000] "POST /files HTTP/1.1" 200 - "-"
      */
     fun find(subString: String) {
+        val paths = Paths.get("io/logs")
+        val resultFile = File("io/result.txt")
 
+        resultFile.outputStream().use { fos ->
+            Files.walk(paths).forEach { path ->
+                if (path.toFile().isFile) {
+                    path.useLines { sequence ->
+                        sequence.filter { line -> line.contains(subString) }
+                            .forEach { string ->
+//                                println("${path.fileName} : ${path.useLines { it.indexOf(string) }} : $string")
+                                fos.write(("${path.fileName} : ${path.useLines { it.indexOf(string) }} : $string\n").toByteArray())
+                            }
+                    }
+                }
+            }
+        }
     }
 }
--- io/src/main/kotlin/ru/sber/nio/Main.kt
+++ io/src/main/kotlin/ru/sber/nio/Main.kt
@@ -2,5 +2,6 @@ package ru.sber.nio
 
 fun main() {
     val grep = Grep()
-    grep.find("22/Jan/2001:14:27:46")
+//    grep.find("22/Jan/2001:14:27:46")
+    grep.find("[22/Jan/2001")
 }
